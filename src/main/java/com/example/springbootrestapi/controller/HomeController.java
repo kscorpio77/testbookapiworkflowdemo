fb7208bdc605b2652ac55e0301ea340d4858fa05
Kundan Sharma <kundan.scorpio@gmail.com>
2026-09-14 17:20:58 +0100

    minor refactoring

--- src/main/java/com/example/springbootrestapi/controller/HomeController.java
+++ src/main/java/com/example/springbootrestapi/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.example.springbootrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//controller class
+
 @RestController
 public class HomeController {
 
