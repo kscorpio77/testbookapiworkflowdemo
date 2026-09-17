e22811e1313e9293c2a958b8df974a28ed1612d2
Kundan Sharma <kundan.scorpio@gmail.com>
2026-09-17 19:21:44 +0100

    comment

--- src/main/java/com/example/springbootrestapi/controller/BookController.java
+++ src/main/java/com/example/springbootrestapi/controller/BookController.java
@@ -21,7 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 @Validated
-/** Controller**/
+/** Controller **/
 public class BookController {
 
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
--- src/main/java/com/example/springbootrestapi/controller/HomeController.java
+++ src/main/java/com/example/springbootrestapi/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.example.springbootrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**controller**/
 @RestController
 public class HomeController {
 
